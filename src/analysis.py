b31e3df55e40464d4c62c9b3e5bfd046dbfd83f6
Luke Kurlandski <lk3591@csec-armitage.main.ad.rit.edu>
2024-06-29 10:58:32 -0400

    Analyzer.

---
+++ src/analysis.py
@@ -0,0 +1,75 @@
+"""
+Analyze the output of the FINNTrainer.
+"""
+
+import json
+from pathlib import Path
+import os
+from typing import Self
+
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib.axes import Axes
+from matplotlib.figure import Figure
+
+
+class FINNTrainerAnalyzer:
+
+    def __init__(self, outfile: Path | os.PathLike, outdir: Path | os.PathLike) -> None:
+        self.outfile = Path(outfile)
+        self.outdir = Path(outdir)
+        self.log: list[dict[str, float]] = []
+        self.data: dict[str, list[float] | np.ndarray] = {}
+
+    def __call__(self) -> Self:
+        self.outdir.mkdir(exist_ok=True, parents=True)
+
+        with open(self.outfile, "r") as fp:
+            for line in fp:
+                self.log.append(json.loads(line))
+
+        if not self.log:
+            raise ValueError("No data found in log file.")
+
+        self.data = {key: [] for key in self.log[0].keys()}
+        for d in self.log:
+            for k, v in d.items():
+                self.data[k].append(v)
+        self.data = {k: np.array(v) for k, v in self.data.items()}
+
+        for k, v in self.data.items():
+            if k == "epoch":
+                continue
+            fig, _ = self.plot(k)
+            fig.savefig(self.outdir / f"{k}.png")
+            plt.close(fig)
+
+        fig, _ = self.plot(["tr_weighted_loss", "vl_weighted_loss", "extraction_rate"])
+        fig.savefig(self.outdir / "main.png")
+        plt.close(fig)
+
+        fig, _ = self.plot([k for k in self.data.keys() if k != "epoch"])
+        fig.savefig(self.outdir / "all.png")
+        plt.close(fig)
+
+    def plot(self, keys: str | list[str]) -> tuple[Figure, Axes]:
+        keys = [keys] if isinstance(keys, str) else keys
+
+        fig, ax = plt.subplots()
+        fig: Figure
+        ax: Axes
+
+        for k in keys:
+            ax.plot(self.data["epoch"], self.data[k], label=k)
+        ax.set_xlabel("Epoch")
+        ax.set_ylabel("Value")
+        ax.set_ylim(0, 1)
+        fig.legend()
+
+        return fig, ax
+
+
+if __name__ == "__main__":
+    for outfile in Path("./output").glob("*.jsonl"):
+        analyzer = FINNTrainerAnalyzer(outfile, outfile.with_suffix(""))
+        analyzer = analyzer()
