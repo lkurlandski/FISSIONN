e3f4067036297c58338d4f6253b70c4410cc4f18
Luke Kurlandski <lk3591@csec-armitage.main.ad.rit.edu>
2024-09-03 11:22:25 -0400

    Not really sure what I chnaged here.

--- src/analysis.py
+++ src/analysis.py
@@ -14,7 +14,7 @@ from matplotlib.axes import Axes
 from matplotlib.figure import Figure
 
 
-class FINNTrainerAnalyzer:
+class Analyzer:
 
     def __init__(self, outdir: Path | os.PathLike) -> None:
         self.outdir = Path(outdir)
@@ -31,18 +31,15 @@ class FINNTrainerAnalyzer:
             raise ValueError("No data found in log file.")
 
         self.data = {key: [] for key in self.log[0].keys()}
-        for k in self.log[1].keys():  # FIXME: remove
-            if k not in self.data:
-                self.data[k] = [np.NaN]
         for d in self.log:
             for k, v in d.items():
                 self.data[k].append(v)
-            
+
         self.data = {k: np.array(v) for k, v in self.data.items()}
 
         return self
 
-    def plot(self) -> Self:
+    def plot(self, keys_for_main: tuple[str] = tuple()) -> Self:
 
         for k, v in self.data.items():
             if k == "epoch":
@@ -51,9 +48,10 @@ class FINNTrainerAnalyzer:
             fig.savefig(self.outdir / f"{k}.png")
             plt.close(fig)
 
-        fig, _ = self._plot(["tr_loss", "vl_loss", "vl_extraction_rate", "vl_bit_error_rate"])
-        fig.savefig(self.outdir / "main.png")
-        plt.close(fig)
+        if keys_for_main:
+            fig, _ = self._plot(keys_for_main)
+            fig.savefig(self.outdir / "main.png")
+            plt.close(fig)
 
         fig, _ = self._plot([k for k in self.data.keys() if k != "epoch"])
         fig.savefig(self.outdir / "all.png")
@@ -71,7 +69,7 @@ class FINNTrainerAnalyzer:
             ax.plot(self.data["epoch"], self.data[k], label=k)
         ax.set_xlabel("Epoch")
         ax.set_ylabel("Value")
-        ax.set_ylim(0, 1)
+        # ax.set_ylim(0, 1)
         fig.legend()
 
         return fig, ax
@@ -81,11 +79,12 @@ def main() -> None:
 
     parser = ArgumentParser()
     parser.add_argument("--outdir", type=Path, required=True)
+    parser.add_argument("--keys_for_main", type=str, nargs="+", default=[])
     args = parser.parse_args()
 
-    analyzer = FINNTrainerAnalyzer(args.outdir)
-    analyzer()
-    analyzer.plot()
+    analyzer = Analyzer(args.outdir)
+    analyzer = analyzer()
+    analyzer.plot(keys_for_main=args.keys_for_main)
 
 
 if __name__ == "__main__":
